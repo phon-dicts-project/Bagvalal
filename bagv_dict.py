6d554de34935074e271a3fd712f19814bc34c74a
Anastasia Davidenko <davidenkonastya98@gmail.com>
2020-05-16 13:53:05 +0300

    Add files via upload

--- bagv_dict.py
+++ bagv_dict.py
@@ -1,79 +1,118 @@
 from bs4 import BeautifulSoup as bs
 import re
 
-##Проблемы:
-##    Слова делятся по слэшам
-##    Слова делятся после акутов (?)
-    
-##&aacute;
-##&ocirc;
-##&yacute;
-##&eacute;
-##&oacute;
-##&#256;
-##&#257;
-##&#268;
-##&#274;
-##&#275;
-##&#332;
-##&#333;
-##&Iuml;
-
 language = 'Bagvalal'
 glottolog = 'bagv1239'
 reference = 'Magomedova 2004' 
 contributor = 'A. Davidenko'
 
-lemma_ipa = ''
-pos = ''
-borrowing = ''
-translation_ru = ''
-translation_en = ''
-
 romanic = ['I', 'II', 'III']
 input_file = 'bagv_dict.html'
-output_file = 'output.csv'
-header = 'id\tlanguage\tglottolog\treference\tlemma_source\tlemma_ipa\tpos\tborrowing\ttranslation_ru\ttranslation_en\tcontributor\n'
+output_file = 'bagv_dict.csv'
+dict_beginning = 521
+dict_ending = 8265
+header = 'id\tlanguage\tglottolog\treference\tlemma_source\tending\tpos\tborrowing\tdialect\ttranslation_ru\tcontributor\tsource\n'
+
+dialect_list = ['(гим.)', '(кван.)', '(тлиб.)', '(тлис.)', '(тлон.)', '(гим)', '(кван)', '(тлиб)', '(тлис)', '(тлон)', '(кван., гим.)']
+
+def preprocessing(text):
+    text = text.replace('<sup>Н</sup>', 'ᴴ')
+    text = text.replace('<sup>н</sup>', 'ᴴ')
+    text = text.replace('<sup>1</sup>', '¹')
+    text = text.replace('<sup>2</sup>', '²')
+    text = text.replace('<sup>2</sup>', '³')
+    return text
+
+def replacing(text):
+    if text[0] == '²':
+        text = text.replace('²', 'Э´')
+    text = text.replace('/²', '/Э´')
+    text = text.replace('\n', ' ')
+    text = text.replace('¯', '̄')
+    text = text.replace('A', 'А')
+    text = text.replace('Ā', 'Ā')
+    text = text.replace('À', 'Á')
+    text = text.replace('ä', 'Б̄')
+    text = text.replace('μ', 'У´')
+    text = text.replace('ЕÁ', 'Л̄Á')
+    text = text.replace('ЕÊ', 'Л̄Á̄')
+    text = text.replace('E', 'Е')
+    text = text.replace('X', 'Х')
+    text = text.replace('R', 'И´')
+    text = text.replace('Ç', 'О´')
+    text = text.replace('N', 'X̄')
+    text = text.replace('Ð', 'С̄Ī')
+    text = text.replace('Æ', 'Ē')
+    text = text.replace('∙', 'У´')
+    text = text.replace('Z', 'Ӯ')
+    text = text.replace('ã', 'М̄')
+    text = text.replace('ÁI', 'ÁЛ̄Ъ̄')
+    text = text.replace('АI', 'АЛ̄Ъ̄')
+    text = text.replace('ç', 'Д̄')
+    text = text.replace(' /', '/')
+    text = text.replace('/ ', '/')
+    text = text.replace(' :', ':')
+    text = text.replace(': ', ':')
+    text = text.replace(' )', ')')
+    text = text.replace('( ', '(')
+    return text
+
+def not_lemma_replacing(text):
+    text = text.replace('B', 'с̄')
+    text = text.replace('S', 'ӣ')
+    text = text.replace('F', 'л̄')
+    text = text.replace('†', '́а̄')    
+    text = text.replace('Н', 'л̄ъ̄')
+    text = text.replace('Þ', 'н̄')
+    text = text.replace('}', 'е́̄')
+    return(text)
 
 with open(input_file, 'r', encoding='utf-8') as f: 
     text = f.read()
-    
+
+text = preprocessing(text)
+
 soup = bs(text,'html.parser')
 lines = soup.find_all('p')
+
 output = open(output_file, 'w', encoding='utf-8')
 output.write(header)
 print('Header is done!')
 out_id = 0
-errors = 0
-for line in lines:
-    try:
-        lemma_source = ''
-        lemma_ipa = ''
-        pos = ''
-        borrowing = ''
-        translation_ru = ''
-        translation_en = ''
-        text = line.get_text()
-        text = text.replace ('\n', ' ')
-        text = text.replace ('(', ' (')
-        splitted_text = text.split()
+
+i = dict_beginning
+while i < dict_ending + 1:
+    lemma_source = ''
+    pos = ''
+    borrowing = ''
+    translation_ru = ''
+    dialect = ''
+    text = replacing(lines[i].get_text())
+        
+    regBrackets = re.compile('\(.+?\)')
+    brackets = regBrackets.findall(text)
+    if len(brackets) > 0:
+        if brackets[0] in dialect_list:
+            dialect = brackets[0]
+            text = text.replace(dialect, '')
+        else:
+            ending = brackets[0]
+            text = text.replace(ending, '')
+            ending = not_lemma_replacing(ending)
+                    
+    splitted_text = text.split()
+    if len(splitted_text) > 1:
         lemma_source = splitted_text[0]
         text = text.replace (splitted_text[0], '')
         if splitted_text[1] in romanic:
             pos = 'noun ' + splitted_text[1]
             text = text.replace (splitted_text[1], '')
-        regEnding = re.compile('\(.+?\)')
-        endings = regEnding.findall(text)
-        if len(endings) > 0:
-            ending = endings[0]
-            text = text.replace (ending, '')
-        translation_ru = text
-        output_line = str(out_id) + '\t' + language + '\t' + glottolog + '\t' + reference + '\t' + lemma_source + ' ' + ending + '\t' + lemma_ipa + '\t' + pos + '\t' + borrowing + '\t' + translation_ru + '\t' + translation_en + '\t' + contributor + '\n'
-        print (output_line)
+        translation_ru = not_lemma_replacing(text)
+        print(lemma_source)
+        str_line = str(lines[i]).replace('\n', ' ')
+        output_line = str(i) + '\t' + language + '\t' + glottolog + '\t' + reference + '\t' + lemma_source + '\t' + ending + '\t' + pos + '\t' + borrowing + '\t' + dialect + '\t' + translation_ru + '\t' + contributor + '\t' + str_line + '\n'
         output.write (output_line)
-        out_id += 1
-    except:
-        output_line = str(out_id) + 'ERROR'
-        errors += 1
+        print('writing to csv ', i)
+    i +=1
+
 output.close()
-print ('Done. Errors: ', errors)
